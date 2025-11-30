072d224c2c63ea507046de0d2bf284c96d266048
Mustafa Kemal <mustafakemalaksoy1@gmail.com>
2025-11-30 17:17:45 +0300

    Add Python script for rounding average of three numbers

---
+++ python_round_function.py
@@ -0,0 +1,14 @@
+
+#degiskenleri alma
+x=float(input("1.sayiyi girin: "))
+y=float(input("2.sayiyi girin: "))
+z=float(input("3.sayiyi girin: "))
+
+#ortalama hesaplama
+
+result=(x+y+z)/3
+
+#yuvarlama ve cıktı işlemi
+
+rounded_result = round(result,1)
+print(f"ortalama : {rounded_result} ")
